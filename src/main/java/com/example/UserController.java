42578bbe46e356ff128438868e52a14d8a8aacdc
Yuriy Shpyluk <yshpyluk@tidemark.com>
2017-04-03 11:00:20 +0300

    first

---
+++ src/main/java/com/example/UserController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by yshpyluk on 4/2/17.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String getMessage(ModelMap modelMap) {
+		modelMap.addAttribute("message", "Hello app world");
+		return "hello";
+	}
+}
